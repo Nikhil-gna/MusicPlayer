494cfc864d6b6e8109fb503c4cc137a908e45f52
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-09-21 22:57:48 +0530

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,50 @@
+import { Track } from "react-native-track-player";
+
+export const playListData: Track[] = [
+    {
+      id: 1,
+      title: 'Night Changes',
+      artist: 'One Direction',
+      album: ' Four',
+      artwork:
+        'https://th.bing.com/th/id/R.8e7dae8c9b8205d8793421c687a67430?rik=BPb1ddgM8KVRSg&riu=http%3a%2f%2fimg1.wikia.nocookie.net%2f__cb20150220031229%2fonedirection%2fimages%2fe%2fee%2fNight_Changes_cover.png&ehk=kRX5b92nxXFaZaZtQj1AkSkTNsBe0EvnJeIgZ53rb9k%3d&risl=&pid=ImgRaw&r=0',
+      url: require('./assets/music/NightChanges.mp3'),
+    },
+    {
+      id: 2,
+      title: 'Sparkle',
+      artist: 'Radwimps',
+      album: 'Your Name',
+      artwork:
+        'https://images.genius.com/d56713c0e012f217d17fe7d0840ccb31.1000x1000x1.png',
+      url: require('./assets/music/Sparkle.mp3'),
+    },
+    {
+      id: 3,
+      title: 'Sunflower',
+      artist: 'Post Malone, Swae Lee',
+      album: 'spiderman into the spider verse',
+      artwork:
+        'https://f4.bcbits.com/img/a0877738317_10.jpg',
+      url: require('./assets/music/Sunflower.mp3'),
+    },
+    {
+      id: 4,
+      title: 'Thunder',
+      artist: 'Imagine Dragons',
+      album: 'Evolve',
+      artwork:
+        'https://images.genius.com/e7768dcddaa060c10a101fb220334640.1000x1000x1.jpg',
+      url: require('./assets/music/Thunder.mp3'),
+    },
+    {
+      id: 5,
+      title: 'Perfect',
+      artist: 'Ed Sheeran',
+      album: 'Dare To Love',
+      artwork:
+        'https://1.bp.blogspot.com/-sAmXohqV1K0/XfcxQeLo9QI/AAAAAAAAGwg/EH6XcKOqKtICnQ3YUaFt64W4oAXY8ClHwCLcBGAsYHQ/s1600/maxresdefault.jpg',
+      url: require('./assets/music/Perfect.mp3'),
+    },
+  ];
+  
